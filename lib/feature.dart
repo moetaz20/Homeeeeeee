e6b21917ad9311055f39b1e7e5a381a5781f8eb2
Moetaz <chahedmoetaez@gmail.com>
2020-02-21 15:27:39 +0100

    created new feature

---
+++ lib/feature.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+class Feature extends StatefulWidget {
+  @override
+  _FeatureState createState() => _FeatureState();
+}
+
+class _FeatureState extends State<Feature> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
